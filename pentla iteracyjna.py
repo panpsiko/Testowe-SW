67935bea82b6eb5e790e27f34d78d4e34be33f99
CSComarch <psiko@gazeta.pl>
2023-02-21 11:06:45 +0100

    cccf

--- pentla iteracyjna.py
+++ pentla iteracyjna.py
@@ -2,7 +2,9 @@
 
 imie = "Adam"
 wiek = 50
-lista = [3, 54, 3, 5, 8, 9]
+lista_2 = [3, 54, 3, 5, 8, 9]
+lista = [3, 54, 3, wiek, 5, lista_2, 8, imie, 9]
 
 for element in lista:
-    print(f"Wartość {element=}")
+    print(f"Wartość {element=} ma typ {type(element)}")
+    print(f"Litera to {chr(element)}")
