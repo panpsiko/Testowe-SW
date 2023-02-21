62a12520456aff66de3693bb52857ec26a753154
CSComarch <psiko@gazeta.pl>
2023-02-21 13:45:13 +0100

    ccaaaaavvv

--- 09.py
+++ 09.py
@@ -4,6 +4,9 @@ colors = ["red", "grean"]
 ile = int(input("Ile chcesz dodać kolorów: "))
 for new in range(ile):
     new_color = input("Jaki chcesz kolor? ")
+    colors.append(new_color)
+
+print(colors)
 
 for color in colors:
     print(f"{color=}")
