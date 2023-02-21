e2f068a2ae73fc2547b8274ad53c706674610e40
CSComarch <psiko@gazeta.pl>
2023-02-21 09:54:41 +0100

    aaaa

---
+++ wtorek 21.py
@@ -0,0 +1,15 @@
+current_year = 2023
+my_bitrthday = 1978
+my_age = current_year - my_bitrthday
+print(f"My age is {my_age}")
+
+if my_age >= 18:
+    print("You are adult")
+    print("hkgkgkghh")
+else:
+    print("You are young")
+elif my_age < 0:
+    print("Not born yet")
+else:
+    print("You are young")
+
