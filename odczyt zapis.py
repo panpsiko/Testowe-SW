6cd8a26d67fc7e2628dc7d0bbfe58bb2363188cb
CSComarch <psiko@gazeta.pl>
2023-02-21 15:03:46 +0100

    bbnnfs

--- odczyt zapis.py
+++ odczyt zapis.py
@@ -1,3 +1,5 @@
+dane ludzi
+
 with open("plik/pliczek_in.txt", "r") as pliki:
     dane = plik.read()
     print(f"{dane=} / {type(dane)}")
@@ -5,6 +7,16 @@ with open("plik/pliczek_in.txt", "r") as pliki:
     with open("plik/pliczek_in.txt", "r") as pliki:
         dane = plik.readline()
         print(f"{dane=} / {type(dane)}")
-
+dane_ludzi = []
 for line in dane:
-    print(f"{line.strip()}")
+    print(f"{line.strip()").split(',')}")
+    danne_ludzi.append(line.strip().split(',') )
+
+    pozycja_z_pliku = line.strip().split(',')
+    nazwisko+ pozycja_z_pliku[0].strip()
+    rok_urodzenia = pozycja_z_pliku[1].strip()
+    print(f"Przetworzone: {nazwisko=} rok: {rok_urodzenia="}
+
+
+
+
