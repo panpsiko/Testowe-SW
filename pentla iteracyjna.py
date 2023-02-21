eb716f3166eb8a0dad7e9efa574e470dc228516f
CSComarch <psiko@gazeta.pl>
2023-02-21 11:30:34 +0100

    cccfd

--- pentla iteracyjna.py
+++ pentla iteracyjna.py
@@ -2,9 +2,13 @@
 
 imie = "Adam"
 wiek = 50
-lista_2 = [3, 54, 3, 5, 8, 9]
+lista_2 = [3, 14, 54, 55]
 lista = [3, 54, 3, wiek, 5, lista_2, 8, imie, 9]
 
 for element in lista:
     print(f"Wartość {element=} ma typ {type(element)}")
     print(f"Litera to {chr(element)}")
+    if isinstance(element, int):
+        print(f"Litera to {chr(element)}")
+    else:
+        print("Sorry")
