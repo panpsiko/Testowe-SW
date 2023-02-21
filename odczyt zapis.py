e489039fedf47f22f277a1897b818b808ad8287a
CSComarch <psiko@gazeta.pl>
2023-02-21 14:49:38 +0100

    bbnnf

--- odczyt zapis.py
+++ odczyt zapis.py
@@ -1,2 +1,10 @@
 with open("plik/pliczek_in.txt", "r") as pliki:
-    dane = plik.re
+    dane = plik.read()
+    print(f"{dane=} / {type(dane)}")
+
+    with open("plik/pliczek_in.txt", "r") as pliki:
+        dane = plik.readline()
+        print(f"{dane=} / {type(dane)}")
+
+for line in dane:
+    print(f"{line.strip()}")
