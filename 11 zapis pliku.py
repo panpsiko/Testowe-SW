f8ecf78c1894567746d33d8311d743e08a989f06
CSComarch <psiko@gazeta.pl>
2023-02-21 14:39:08 +0100

    bbnnf

--- 11 zapis pliku.py
+++ 11 zapis pliku.py
@@ -9,3 +9,11 @@ for preson in klasa:
 
 for person_2, year in klasa:
     print(f"{person_2=} ->> {year}")
+
+
+# zapis do pliku
+
+with open("pliczek.txt", "w") as plik:
+    for person in klasa:
+    plik.write(str(person) + chr(10))
+
---
+++ odczyt zapis.py
@@ -0,0 +1,2 @@
+with open("plik/pliczek_in.txt", "r") as pliki:
+    dane = plik.re
