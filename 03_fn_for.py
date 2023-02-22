a7a38cfa03a60a06ca54d3adee4d7ed625b37d99
CSComarch <psiko@gazeta.pl>
2023-02-22 10:09:05 +0100

    03_fn_for

---
+++ 03_fn_for.py
@@ -0,0 +1,10 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def pisz_literki(litera1, litera2):
+    print(litera1 +" -> " + litera2)
+
+for ind, litera in enumerate(alphabet):
+    pisz_literki(str(ind), litera)
+
+
+
