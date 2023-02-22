d1803ce5bf2a702d509fd35187614065719da242
CSComarch <psiko@gazeta.pl>
2023-02-22 10:23:22 +0100

    02_funkcje

--- 02.funkcje.py
+++ 02.funkcje.py
@@ -1,18 +1,24 @@
+# petla iteracyjna + iterables
 
+def info_znak(liczba):
+    # f"Litera to {chr(element)}"
+    znak = chr(liczba)
+    co_zwracam = f"Litera to {znak}"
+    return co_zwracam
 
 imie = "Adam"
 wiek = 49
-lista_2 = [3.14, 55, 54]
+lista_2 = [3,14, 55, 54]
 lista = [63, 54, 93, wiek, 45, lista_2, 8, imie, 9]
 
 for element in lista:
     print(f"Wartość {element=} ma typ {type(element)}")
     if isinstance(element, int):
-        print(f"Litera to {chr(element)}")
+        print(info_znak(element))
     elif isinstance(element, list):
         print(f"Teraz lista wewnętrzna - {element}")
         for inny in element:
-            print(f"Litera wewnętrzna to {chr(inny)}")
+            print(info_znak(inny))
     elif isinstance(element, str):
         print(f"Teraz string wewnętrzna - {element}")
         for inny in element:
