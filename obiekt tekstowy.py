1ae7c5a9adfa980ec55a82bcc947e949f3d5e6c4
CSComarch <psiko@gazeta.pl>
2023-02-22 11:11:36 +0100

    moduły

---
+++ obiekt tekstowy.py
@@ -0,0 +1,18 @@
+zmienna1 = "Ala ma kota"
+zmienna2 = "a kot ma Alę"
+
+zmienna3 = """
+Ala ma kota
+a kot ma Alę
+a tu inna linia 
+   i jeszcze z wcięciem
+i bez wcięcia
+"""
+
+# komentarz wielolinijkowy
+"""
+To jest komentarz do programu.
+Jest to dana bez zmiennej
+"""
+
+print(zmienna3)
