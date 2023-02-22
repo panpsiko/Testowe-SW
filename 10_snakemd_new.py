2bb01f687a5e158f21b16918737b0c3c55ee7c31
CSComarch <psiko@gazeta.pl>
2023-02-22 15:31:47 +0100

    moduły ex

--- 10_snakemd_new.py
+++ 10_snakemd_new.py
@@ -1,6 +1,9 @@
 import snakemd
+from secrets import token_urlsafe # https://docs.python.org/3/library/secrets.html
 
-nowy_dokument = snakemd.new_doc("pliki/nasz_nowy_plik")
+plik = f"pliki/{token_urlsafe(8)}"
+nowy_dokument = snakemd.new_doc(plik)
+
 
 nowy_dokument.output_page()
 
