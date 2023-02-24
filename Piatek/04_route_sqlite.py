01c7c755bd2d86b0acec0d84d7fe099876ae52ff
CSComarch <psiko@gazeta.pl>
2023-02-24 11:41:13 +0100

    moduły ex3

---
+++ Piatek/04_route_sqlite.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+DATABASE = "funkcje_dodatkowe/users.db"
+
+from funkcje_dodatkowe.baza_danych import generate_token, create_user_record
+from funkcje_dodatkowe.send_email_smarthost import mail_report
+from datetime import datetime
+
+app = Flask("moja_apka")
+
+
+@app.route("/") # dekorator
+def main_page():
+    return "<H1> Welcome</H1>"
+
+@app.route("/data")
+def get_data():
+    html = """
+    <h2> Podaj nam dane </h2> <hr>
+    Tu chcę Twój adres email: ...... <hr>
+    """ + str(generate_token())
+    return html
+
+@app.route("/user/<value>")
+def username(value):
+    html = f"""
+    <H1>Welcome new user</H1>
+    Wysyłam ci maila na adres: {value}
+    """
+    new_token = generate_token()
+    mail_body = f"treść naszego maila - {datetime.today()} - {new_token}"
+    if value.count("@") == 1:
+        ret, ret_value = mail_report(value,"python-course@jurkiewicz.tech", mail_body)
+        if ret == False:
+            print(ret_value)
+        if ret == True:
+            retdb = create_user_record(DATABASE, value, new_token)
+            print(f"Returned: {retdb}")
+    return html
+
+app.run()
--- Piatek/funkcje_dodatkowe/baza_danych.py
+++ Piatek/funkcje_dodatkowe/baza_danych.py
@@ -1,6 +1,8 @@
 from secrets import token_hex
 from sqlite3 import *
+import os
 print(f"blah blah blah - {__name__} / To jest przetwarzane przy jakimkolwiek imporcie")
+
 
 # https://sqlitebrowser.org/
 
@@ -8,3 +10,19 @@ def generate_token():
     new_token = token_hex(60)
     print(new_token)
     return new_token
+
+def create_user_record(db, user, token):
+    field_1 = "mail"
+    field_2 = "token"
+    new_values = (user, token)
+    if os.path.exists(db):
+        connection = connect(db)
+        cursor = connection.cursor()
+        cursor.execute(f'INSERT INTO userdata ({field_1},{field_2}) VALUES (?,?)', new_values)
+        connection.commit()
+        connection.close()
+        return True
+    else:
+        return False
+
+
